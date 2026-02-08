c9552bc676846a33d8f530b07731cad89b4fd8cc
Bánfalvi-Kovács Tivadar <bktb888@icloud.com>
2026-02-08 15:51:10 +0100

    1. feladat

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use subxt::{OnlineClient, PolkadotConfig};
+use subxt::utils::AccountId32;
+use std::str::FromStr;
+
+#[subxt::subxt(runtime_metadata_path = "metadata.scale")]
+pub mod runtime {}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let api = OnlineClient::<PolkadotConfig>::from_url(
+        "wss://testnet-gw1.mosaicchain.io/testnet-blockchain-1/chain"
+    ).await?;
+
+    let account =
+        AccountId32::from_str("14s3KFN3AHnQ8xji3cd7BEMzF4ciipNRv3azgQwjFrf5seaW")?;
+
+
+    /// 1. Feladat
+    let account_info = api
+        .storage()
+        .at_latest()
+        .await?
+        .fetch(&runtime::storage().system().account(account))
+        .await?
+        .expect("Account not found");
+
+    println!("{:#?}", account_info.data);
+
+    Ok(())
 }
